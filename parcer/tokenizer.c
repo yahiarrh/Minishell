4e4b0c413f53eb95ea828b76000d7d7d84154d9e
Saidi Mohamed-Amine <106715780+msaidi01@users.noreply.github.com>
2023-09-03 15:16:40 +0100

    Revert "Tehadden 3afak"

--- parcer/tokenizer.c
+++ parcer/tokenizer.c
@@ -1,48 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tokenizer.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: msaidi <msaidi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/03 08:10:01 by msaidi            #+#    #+#             */
-/*   Updated: 2023/09/03 14:59:51 by msaidi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "tokenizer.h"
-int	spc_chk(char c)
+#include <stdio.h>
+int main ()
 {
-	if (c && (c == ' ' || c == '\t'))
-		return (1);
-	return (0);
-}
-void	indicator_check(char *arg, t_list *command)
-{
-	
-
-}
-
-void	word_tok(char *arg, t_list *command)
-{
-
-}
-
-t_list	tokenizer(char *arg)
-{
-	t_list	*command;
-	int		i;
-
-	i = -1;
-	command = malloc(sizeof (*t_list));
-	while (arg[++i])
-	{
-		while (arg[i] && arg[i] != ' ' && arg[i] != '\t')
-			i++;
-		if (ft_strchr("|\'\"<>", arg[i]) || spc_check(arg[i]))
-			indicator_check(arg + i, command);
-		else
-			word_tok(arg + i, command);
-		
-	}
+    printf("hello\n");
 }
