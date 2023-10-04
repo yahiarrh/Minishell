bee1e7c7dc1a4ccac096fec8cf02efe754485f2a
yrrhaibi <yahiarrh@gmail.com>
2023-10-04 14:43:33 +0100

    prob exp finished

--- expander/exp_utils.c
+++ expander/exp_utils.c
@@ -6,7 +6,7 @@
 /*   By: yrrhaibi <yrrhaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:37:53 by yrrhaibi          #+#    #+#             */
-/*   Updated: 2023/10/04 13:54:07 by yrrhaibi         ###   ########.fr       */
+/*   Updated: 2023/10/04 14:43:12 by yrrhaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void	cmd_back(t_cmd **lst, t_cmd *new)
 		*lst = new;
 		return ;
 	}
-	ptr = cmdlast(*lst);
+	ptr = cmd_last(*lst);
 	ptr->next = new;
 }
 static void	sq_case(t_cmd **head, t_token *token)
@@ -42,7 +42,7 @@ static void	sq_case(t_cmd **head, t_token *token)
 	tmp = get_ptr(sizeof(t_cmd), ALLOC);
 	ft_memset(tmp, 0, sizeof(t_cmd));
 	tmp->cmd = ft_strdup(token->word);
-	cmd_back(&head, tmp);
+	cmd_back(head, tmp);
 }
 static void	dq_case(t_env **env, t_cmd **head, t_token *token)
 {
@@ -51,20 +51,20 @@ static void	dq_case(t_env **env, t_cmd **head, t_token *token)
 	tmp = get_ptr(sizeof(t_cmd), ALLOC);
 	ft_memset(tmp, 0, sizeof(t_cmd));
 	tmp->cmd = expand(env, token->word);
-	cmd_back(&head, tmp);
+	cmd_back(head, tmp);
 }
 static void	wrd_case(t_env **env, t_cmd **head, t_token *token)
 {
 	char	**s;
 	t_cmd	*tmp;
 
-	s = ft_split(expand, ' ');
+	s = ft_split(expand(env, token->word), ' ');
 	while (*s)
 	{
 		tmp = get_ptr(sizeof(t_cmd), ALLOC);
 		ft_memset(tmp, 0, sizeof(t_cmd));
 		tmp->cmd = *s;
-		cmd_back(&head, tmp);
+		cmd_back(head, tmp);
 		s++;
 	}
 }
